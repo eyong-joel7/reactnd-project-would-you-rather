7fc82c59d3e3da071463aec95f2ccc30667a8805
JOEL EYONG <62267332+eyong-joel7@users.noreply.github.com>
2021-10-11 19:29:57 +0300

    Fix: poll question unsorted - resolved

--- src/components/PollList.js
+++ src/components/PollList.js
@@ -34,8 +34,8 @@ class PollList extends Component {
       : [];
 
     return {
-      answered_questions,
-      unAnswered_questions,
+      answered_questions : answered_questions.sort((a,b) => b.timestamp - a.timestamp),
+      unAnswered_questions:unAnswered_questions.sort((a,b) => b.timestamp - a.timestamp),
     };
   };
 
@@ -51,7 +51,12 @@ class PollList extends Component {
 
     const { answered_questions, unAnswered_questions } =
       this.getGroupedQuestions(answers_keys, questions_keys, questions);
-
+      console.log(answered_questions, unAnswered_questions)
+//  const sorted_answered_questions = [];
+//  const sorted_unAnswered_questions = [];
+//  for(let q in answered_questions){
+//    sorted_answered_questions.push(answered_questions[q])
+//  }
     const activeClass = this.state.activeClass;
     return (
       <div className="list-card">
